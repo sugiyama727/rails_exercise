ac14f5734c3d0b1190b4dc6afa9fa7ab4950c45e
杉山　ちひろ <ska69nknk@icloud.com>
2020-03-23 23:56:54 +0900

    [Finish]3-6

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,29 @@
+class TodolistsController < ApplicationController
+
+  def new
+  	@list = List.new
+  end
+
+  def index
+  	@lists = List.all
+  end
+
+  def create
+  	# ストロングパラメーターを使用
+  	list = List.new(list_params)
+  	# DBへ保存する
+  	list.save
+  	# トップ画面へリダイレクト
+  	redirect_to todolist_path(list.id) # 詳細画面へリダイレクト
+  end
+
+  def show
+  	@list = List.find(params[:id])
+  end
+
+  private
+
+  def list_params
+  	params.require(:list).permit(:title, :body)
+  end
+end
