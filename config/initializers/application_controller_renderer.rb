500987ac1afb51e2c3686addaf4002ffb59a083a
杉山　ちひろ <ska69nknk@icloud.com>
2020-03-24 15:54:14 +0900

    [Finish]3-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1dd4d63459fda6e4169817e189166cf9bf5d23dd55dca76df15592940c480cf92157fa99a8fc306397898c77764b7f5983f90962a3d320dc66abafb95624d250'
